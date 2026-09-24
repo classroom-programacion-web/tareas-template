673bdb4af9ee5511fd7bb14b5c3d63e60cab4970
GeyslerAh-kinYamOlvera <geysler2004@gmail.com>
2026-09-24 16:21:54 -0500

    Update app.js

--- sprint-2/ejercicio-1-objetos-arreglos/app.js
+++ sprint-2/ejercicio-1-objetos-arreglos/app.js
@@ -7,6 +7,26 @@ const usuario = {
   },
 };
 
+//[camioneta,sedan]
+
+//camioneta:{
+// "clave:"llanta"
+// "clave:"volante"
+// "clave:"puerta"
+// "clave:"bal"
+// "clave:""
+//}
+
+//sedan:{
+// "clave:"llanta"
+// "clave:"volante"
+// "clave:"puerta"
+// "clave:"bal"
+// "clave:""
+//}
+
+
+usuario.sprintActivo;
 usuario.presentarse();
 
 const proyectos = [
@@ -17,11 +37,13 @@ const proyectos = [
 ];
 
 console.log("Catálogo inicial de proyectos:");
+
 console.table(proyectos);
 
 const nombresDeEquipos = proyectos.map(function (proyecto) {
   return proyecto.equipo;
 });
+
 
 console.log("Nombres de los equipos:", nombresDeEquipos);
 
@@ -29,21 +51,97 @@ const proyectosDestacados = proyectos.filter(function (proyecto) {
   return proyecto.calificacion >= 9;
 });
 
+console.log(proyectosDestacados);
+
 console.log("Proyectos con calificación >= 9:");
 console.table(proyectosDestacados);
 
-const contenedor = document.getElementById("resultado-dom");
 
-function pintarProyectoConEventos(lista) {
-  contenedor.innerHTML = "";
 
-  lista.forEach(function (proyecto) {
+// ACTIVIDAD 1
+
+function crearPerfil(ropa, ColorPelo, Mascota, colorZapato, GeneroMusical) {
+  return {
+    ropa: ropa,
+    colorPelo: ColorPelo,
+    mascota: Mascota,
+    colorZapato: colorZapato,
+    generoMusical: GeneroMusical,
+  };
+}
+
+const miPerfil = crearPerfil("Deportivo", "Negro", "Perro salchicha", "Azul;", "Reggae");
+
+console.log("Mi perfil:");
+console.log(miPerfil);
+
+const perfiles = [
+  crearPerfil("Deportivo", "Negro", "Perro salchicha", "Azul", "Reggae"),
+];
+
+console.log("Mi perfil:");
+console.table(perfiles);
+
+
+
+
+//Ejercicio clase
+
+const contenerdor = document.getElementById("resultado-dom");
+
+function pintarpropyecto (lista){
+  contenerdor.innerHtml = "";
+
+  lista.forEach(function(proyecto){
+  const tarjeta = document.createElement("div");
+
+  //ccc
+  tarjeta.style.border="10px solid #098bf6";
+  tarjeta.style.padding="10px";
+  tarjeta.style.margin="10px";
+  
+  //html
+  tarjeta.innerHTML = `
+  <h3> hola yo soy Geysler </h3>
+  <h3> yo soy del equipo de playstation </h3>
+  <h3> a mi megusta el volley </h3>
+  `;
+
+  tarjeta.onmouseover = function(){
+    tarjeta.style.backgroundColor = "green";
+    tarjeta.style.transform = "scale(1.02)";
+  };
+
+  tarjeta.onmouseout = function(){
+    tarjeta.style.background = "red";
+    tarjeta.style.transform = "scale(1)";
+  };
+
     // <--- TAREA T2-SPRING#2: EVENTOS SIMPLES --->
     // Instrucción: Agrega un evento de "doble clic" (ondblclick) a la tarjeta.
     // Cuando el usuario haga doble clic, la tarjeta debe desaparecer.
     // <--- AQUI TU CÓDIGO --->
-    // <--- FIN DE TU CÓDIGO --->
+  tarjeta.ondblclick = function(){
+    tarjeta.remove();
+  };
+  // <--- FIN DE TU CÓDIGO --->
+
+   const tarjetaHtml = `
+  <div style="border: 1px solid #ccc;">
+  <h1>${proyecto.titulo} </h1>
+  <p>${proyecto.equip}</p>
+  <p>${proyecto.calificacion}</p>
+
+  </div>
+
+   `;
+
+  
+   
+   contenerdor.appendChild(tarjeta);
+   //contenerdor.innerHTML += tarjetaHtml;
   });
+
 }
 
-pintarProyectoConEventos(proyectos);
+pintarpropyecto(proyectos);
